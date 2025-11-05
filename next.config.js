3abb0f36203404b966524a203a1c5ce3cde852d4
Fikri Eren <fikri.eren@bkmkitap.com>
2025-11-05 09:39:59 +0300

    fix: add webpack alias for @ path resolution in Netlify
    - Add webpack config to resolve @ alias to src directory
    - This should fix Module not found errors in Netlify build
    - Ensures TypeScript path mapping works in production build

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -12,6 +14,13 @@ const nextConfig = {
   },
   experimental: {
     esmExternals: 'loose'
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    }
+    return config
   }
 }
 
